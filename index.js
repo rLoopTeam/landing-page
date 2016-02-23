d0570c2708707e3e91ffb7d8a8ab468b3fe2de97
Ari Porad <ari@ariporad.com>
2016-02-23 06:52:51 -0800

    feat(form): email form now submits to MailChimp!

--- index.js
+++
@@ -1,7 +0,0 @@
-;(function(document, undefined){
-  var form = document.getElementById('form');
-  form.onsubmit = function(e) {
-    e.preventDefault();
-    alert('Joined Email List: ' + document.getElementById('email').value);
-  };
-})(document, void 0);
